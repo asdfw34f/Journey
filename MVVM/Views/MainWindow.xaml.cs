0348fb9ac92805e7bfa8b4c83cbf6f11e4c30867
Daniil Demekhin <daniil2002.d11@gmail.com>
2023-06-17 00:52:29 +0300

    Добавьте файлы проекта.

---
+++ MVVM/Views/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using Journey;
+
+namespace Journey.MVVM.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
