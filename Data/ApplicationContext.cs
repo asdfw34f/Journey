a7ba2ba11be22a145386bee82457934455891144
Daniil Demekhin <daniil2002.d11@gmail.com>
2023-06-18 00:08:53 +0300

    User table

---
+++ Data/ApplicationContext.cs
@@ -0,0 +1,20 @@
+﻿using Journey.MVVM.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Journey.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<User> Users { get; set; } = null;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=DESKTOP-9N46EPK\DANIIL_BANK1230;Database=Journy;Trusted_Connection=True;");
+        }
+    }
+}
---
+++ MVVM/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Journey.MVVM.Models
+{
+    public class User
+    {
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+        public string? Name { get; set; }
+        public string? Surename { get; set; }
+        public DateTime? Date { get; set; }
+        public BinaryData Image { get; set; }
+    }
+}
